803f258f1118e4e99c906cbfac1a47e64ec00b94
Ajiue23 <triajipramuditya23@gmail.com>
2025-04-02 18:25:48 +0700

    perubahan

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,14 @@
-import type React from "react"
-import "@/app/globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import type React from "react";
+import "@/app/globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata = {
-  title: "Jane Smith - UI/UX Designer & Developer",
-  description:
-    "Portfolio of Jane Smith, a UI/UX Designer and Developer specializing in creating beautiful, functional digital experiences.",
-    generator: 'v0.dev'
-}
+  title: "Tri Aji Pramuditya - UI/UX Designer & Developer",
+  description: "Portfolio of Tri Aji Pramuditya, a UI/UX Designer and Developer specializing in creating beautiful, functional digital experiences.",
+  generator: "v0.dev",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
@@ -22,9 +17,7 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
 
-
-
-import './globals.css'
+import "./globals.css";
